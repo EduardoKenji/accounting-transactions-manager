d6c4c4853388dd64ec2b40117783879dba23c2c5
EduardoKenji <eduardo.koketu@gmail.com>
2022-10-30 22:12:00 -0300

    Add more model tests to check for zero or negative amount

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -17,6 +17,14 @@ RSpec.describe Transaction, type: :model do
     expect(Transaction.new(credit_account: 'c123456', debit_account: 'd123456', amount: nil)).to_not be_valid
   end
 
+  it 'is not created with invalid amount as 0' do
+    expect(Transaction.new(credit_account: 'c123456', debit_account: 'd123456', amount: 0)).to_not be_valid
+  end
+
+  it 'is not created with invalid amount as negative' do
+    expect(Transaction.new(credit_account: 'c123456', debit_account: 'd123456', amount: -100)).to_not be_valid
+  end
+
   it 'is not created with invalid credit_account as a lengthy (>= 16) string' do
     expect(Transaction.new(credit_account: 'c123456789123456789', debit_account: 'd123456',
                            amount: 50)).to_not be_valid
